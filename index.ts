f0947c095d05610558e59207b51a155a50ee99cc
George Bora <bora.george@yahoo.com>
2016-12-11 17:38:12 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+class Point {
+    X: number;
+    Y: number;
+    Z: number;
+
+    constructor(X: number, Y: number, Z: number) {
+        this.X = X;
+        this.Y = Y;
+        this.Z = Z;
+    }
+}
+
+export default Point;
